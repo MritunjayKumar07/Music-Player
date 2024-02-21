e881ce53f18ebe3a7ffc2196f280a00934a88076
[username] <[user>
2024-02-21 11:50:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Playlist from "./components/Playlist";
+import AudioPlayer from "./components/AudioPlayer";
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [currentFileIndex, setCurrentFileIndex] = useState(0);
+
+  useEffect(() => {
+    const storedFiles = JSON.parse(localStorage.getItem("audioFiles")) || [];
+    setFiles(storedFiles);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("audioFiles", JSON.stringify(files));
+  }, [files]);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setFiles([...files, file]);
+    saveAudioFile([...files, file]);
+  };
+
+  const saveAudioFile = (files) => {
+    localStorage.setItem("audioFiles", JSON.stringify(files));
+  };
+
+  const handleFileSelected = (file) => {
+    const index = files.findIndex((f) => f === file);
+    setCurrentFileIndex(index);
+  };
+
+  const handleFileEnded = () => {
+    setCurrentFileIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const currentFile = files[currentFileIndex];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="boxes">
+        {currentFile && (
+          <>
+            <Playlist files={files} onFileSelected={handleFileSelected} />
+            <AudioPlayer
+              src={URL.createObjectURL(currentFile)}
+              onEnded={handleFileEnded}
+              playName={currentFile}
+            />
+          </>
+        )}
+        <div id="player03" className="player horizontal">
+          <h3>Upload Files</h3>
+          <div className="wrapper upload">
+            <div className="drop_box">
+              <header>
+                <h4>Select File here</h4>
+              </header>
+              <p>Files Supported: MP3, MP4, M4A, WMA</p>
+              <input
+                type="file"
+                hidden
+                accept=".mp3,.mp4,.m4a,.wma"
+                id="fileID"
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+              />
+              <label htmlFor="fileID" className="btn">
+                Choose File
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Playlist.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import image from "../assert/music.png";
+
+function Playlist({ files, onFileSelected }) {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  return (
+    <div id="player01" className="player">
+      <h3 className="info" style={{ textAlign: "center" }}>
+        <h1>Playlist</h1>
+      </h3>
+      <div className="wrapper">
+        <ul className="list-group">
+          {files.map((file, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                setSelectedIndex(file.name);
+                onFileSelected(file);
+              }}
+              style={{
+                backgroundColor:
+                  selectedIndex === file.name ? "#fff" : "transparent",
+                color: selectedIndex === file.name ? "#000" : "#fff",
+                padding: "5px",
+                borderRadius: "8.5px",
+              }}
+            >
+              <img
+                src={image}
+                alt="music icon"
+                style={{ marginRight: "16px" }}
+              />
+              <h3>{file.name}</h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Playlist;
